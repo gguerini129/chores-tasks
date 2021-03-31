6fd577f98d997d282e168e048213413b2f94ce6b
Nicholas Klapatch <nicholasklapatch99@gmail.com>
2021-03-30 23:23:12 -0400

    Added additional tables to the database (task_list and parent)

--- user.sql
+++ user.sql
@@ -1,10 +1,25 @@
-CREATE TABLE `user` (
-  `user_id` int(16) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
-  `email` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
-  `first_name` varchar(32) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `last_name` varchar(32) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE user (
+  user_id int(16) NOT NULL AUTO_INCREMENT,
+  username varchar(32) NOT NULL,
+  password varchar(32) NOT NULL,
+  email varchar(64) NOT NULL,
+  first_name varchar(32),
+  last_name varchar(32),
+  PRIMARY KEY (user_id),
+  UNIQUE KEY (username)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE task_list (
+  task_list_id int(16) NOT NULL AUTO_INCREMENT,
+  name varchar(32) NOT NULL,
+  PRIMARY KEY (task_list_id)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE parent (
+  parent_id int(16) NOT NULL AUTO_INCREMENT,
+  user_id int(16) NOT NULL,
+  task_list_id int(16) NOT NULL,
+  PRIMARY KEY (parent_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id),
+  FOREIGN KEY (task_list_id) REFERENCES task_list(task_list_id)
+) AUTO_INCREMENT=1;
